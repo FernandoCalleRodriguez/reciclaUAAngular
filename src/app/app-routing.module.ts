f3663b3d56490cff9213335fc00e5eeb30cef3dc
Agulló <joseantonioagullo@gmail.com>
2020-04-10 19:00:52 +0200

    Nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import {ListwebComponent} from './web/listweb/listweb.component';
 import {CrearwebComponent} from './web/crearweb/crearweb.component';
 import {ModificarwebComponent} from './web/modificarweb/modificarweb.component';
 import {UsuarioComponent} from './usuario/usuario.component';
+import {ListaDudasComponent} from './foro/lista-dudas/lista-dudas.component';
+import {RespuestasComponent} from './foro/respuestas/respuestas.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -22,6 +24,10 @@ const routes: Routes = [
   {path: 'usuarioweb/modificar/:usuarioId', component: ModificarwebComponent},
 
   {path: 'usuario/:usuarioId', component: UsuarioComponent, pathMatch: 'full'},
+
+  {path: 'foro/duda/listar', component: ListaDudasComponent},
+  {path: 'foro/duda/:dudaId/respuestas', component: RespuestasComponent},
+  {path: 'foro/respuesta/listar', component: RespuestasComponent},
 
   {path: '**', redirectTo: ''}
 ];
