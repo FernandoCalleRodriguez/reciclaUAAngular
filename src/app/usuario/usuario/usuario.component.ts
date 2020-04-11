7710166e320f83b090656fa7a9eaa15e69663c26
ferky_27 <ferky_27@hotmail.com>
2020-04-11 11:11:03 +0200

    Modificación usuario comonent
    Comvertido usuario componente en gestión del perfil de usuario

--- src/app/usuario/usuario/usuario.component.ts
+++ src/app/usuario/usuario/usuario.component.ts
@@ -1,34 +1,47 @@
 import {Component, OnInit} from '@angular/core';
 import {UsuarioService} from '../../shared/services/usuario.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Usuario} from '../../shared/models/usuario';
+import {AutenticacionService} from '../../shared/services/autenticacion.service';
 
 @Component({
-  selector: 'app-usuario',
-  templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+    selector: 'app-usuario',
+    templateUrl: './usuario.component.html',
+    styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent implements OnInit {
-  tipousuario: string;
-  usuarioId: string;
-  usuario: Usuario;
+    tipousuario: string;
+    usuarioId: string;
+    usuario: Usuario;
+    perfil = false;
 
-  constructor(protected route: ActivatedRoute,
-              private usuarioService: UsuarioService) {
-    this.route.params.subscribe(param => {
+    constructor(protected route: ActivatedRoute,
+                private usuarioService: UsuarioService,
+                private  autenticacionService: AutenticacionService,
+                private router: Router) {
+        this.route.params.subscribe(param => {
 
-      this.tipousuario = param['tipousuario'];
-      this.usuarioId = param['usuarioId'];
+            this.tipousuario = param['tipousuario'];
+            this.usuarioId = param['usuarioId'];
 
-      this.usuarioService.obtenerUsuarioPorId(this.usuarioId, this.tipousuario).subscribe(usuario => {
-        this.usuario = usuario;
+            this.usuarioService.obtenerUsuarioPorId(this.usuarioId, this.tipousuario).subscribe(usuario => {
+                this.usuario = usuario;
 
-      });
-    });
+                if (this.autenticacionService.getID() == this.usuarioId) {
+                    this.perfil = true;
+                }
+            });
+        });
 
-  }
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+
+    borrarUsuario() {
+        this.usuarioService.borrarUsuario(this.usuarioId, this.tipousuario).subscribe(res => {
+            this.router.navigate(['/listarusuario/' + this.tipousuario]);
+        });
+    }
 }
