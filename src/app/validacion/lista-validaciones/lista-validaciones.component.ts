ebf7cec2bd9b087e102e7464c57a11b67ff65722
Agulló <joseantonioagullo@gmail.com>
2020-04-13 18:46:44 +0200

    Implementados getters en el componente de validación

--- src/app/validacion/lista-validaciones/lista-validaciones.component.ts
+++ src/app/validacion/lista-validaciones/lista-validaciones.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ValidacionService} from '../../shared/services/validacion.service';
+import {Material} from '../../shared/models/material';
+import {Punto} from '../../shared/models/punto';
+import {Item} from '../../shared/models/item';
+import {TipoContenedorService} from '../../shared/services/tipo-contenedor.service';
+import {TipoContenedor} from '../../shared/models/contenedor';
 
 @Component({
   selector: 'app-lista-validaciones',
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-validaciones.component.css']
 })
 export class ListaValidacionesComponent implements OnInit {
+  public puntos: Punto[] = null;
+  public items: Item[] = null;
+  public materiales: Material[] = null;
 
-  constructor() { }
+  constructor(protected validacionService: ValidacionService, protected tipoContenedorService: TipoContenedorService) {
+    validacionService.getAllPuntosSinValidar().subscribe(p => {
+      this.puntos = p;
+    });
+    validacionService.getAllItemsSinValidar().subscribe(i => {
+      this.items = i;
+    });
+    validacionService.getAllMaterialesSinValidar().subscribe(m => {
+      this.materiales = m;
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  public getTipoContenedor(id: number): TipoContenedor {
+    return this.tipoContenedorService.getTipoById(id);
+  }
 }
