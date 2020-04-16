18e681d813d7a267db619af07a5283ac41c1a8c5
ferky_27 <ferky_27@hotmail.com>
2020-04-16 09:37:46 +0200

    Adaptar estilo tabla a jose

--- src/app/usuario/listarusuario/usuariolistar.component.ts
+++ src/app/usuario/listarusuario/usuariolistar.component.ts
@@ -38,22 +38,6 @@ export class UsuariolistarComponent implements OnInit {
 
   }
 
-  onSearch() {
-    console.log('entro');
-
-    this.usuarioService.obtenerUsuarioPorId(this.formularioBuscar.value.id, this.tipousuario).subscribe(usuario => {
-        this.usuarios.splice(0);
-        if (usuario != null) {
-          this.usuarios.push(usuario);
-          console.log(this.usuarios);
-        }
-
-      },
-      error => {
-        this.usuarios.splice(0);
-
-      });
-  }
 
   borrarUsuario(id) {
     this.usuarioService.borrarUsuario(id, this.tipousuario).subscribe(res => {
@@ -63,6 +47,10 @@ export class UsuariolistarComponent implements OnInit {
 
   modificarUsuario(id) {
     this.router.navigate(['/modificarusuario/' + this.tipousuario + '/' + id]);
+  }
+
+  mostrarUsuario(id) {
+    this.router.navigate(['/usuario/' + this.tipousuario + '/' + id]);
   }
 
 }
