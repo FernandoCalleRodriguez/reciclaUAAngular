63e452198945e1b7a945ec7289f945a6a06daa94
Agulló <joseantonioagullo@gmail.com>
2020-04-16 18:52:16 +0200

    Añadidos detalles de respuesta y borrar (modales)

--- src/app/foro/lista-respuestas/lista-respuestas.component.ts
+++ src/app/foro/lista-respuestas/lista-respuestas.component.ts
@@ -4,6 +4,10 @@ import {Respuesta} from '../../shared/models/respuesta';
 import {RespuestaService} from '../../shared/services/respuesta.service';
 import {DudaService} from '../../shared/services/duda.service';
 import {Duda} from '../../shared/models/duda';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ToastrService} from 'ngx-toastr';
+import Swal from 'sweetalert2';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-respuestas',
@@ -15,9 +19,11 @@ export class ListaRespuestasComponent implements OnInit {
   public respuesta: Respuesta = null;
   public duda: Duda = null;
   public dudaId: number = null;
+  public dtTrigger: Subject<any> = new Subject();
 
   constructor(protected respuestaService: RespuestaService, protected dudaService: DudaService,
-              protected router: Router, protected route: ActivatedRoute) {
+              protected router: Router, protected route: ActivatedRoute,
+              protected modalService: NgbModal, protected toaster: ToastrService) {
     this.route.params.subscribe(params => {
       if (params.dudaId) {
         this.dudaId = params.dudaId;
@@ -35,11 +41,12 @@ export class ListaRespuestasComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  showRespuesta(respuesta: Respuesta) {
+  showRespuesta(respuesta: Respuesta, detail) {
     this.respuesta = respuesta;
     this.dudaService.getDudaByRespuesta(respuesta.Id).subscribe(d => {
       this.duda = d;
     });
+    this.modalService.open(detail, {size: 'xl'});
   }
 
   editRespuesta(respuesta: Respuesta) {
@@ -49,13 +56,25 @@ export class ListaRespuestasComponent implements OnInit {
   }
 
   deleteRespuesta(respuesta: Respuesta) {
-    if (confirm('¿Seguro que deseas borrar esta respuesta?')) {
-      this.respuestaService.borrar(respuesta).subscribe(() => {
-        const index = this.respuestas.indexOf(respuesta);
-        if (index > -1) {
-          this.respuestas.splice(index, 1);
-        }
-      });
-    }
+    Swal.fire({
+      title: '¿Estás seguro de que deseas borrar la respuesta ' + respuesta.Id + '?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.respuestaService.borrar(respuesta).subscribe(() => {
+          const index = this.respuestas.indexOf(respuesta);
+          if (index > -1) {
+            this.respuestas.splice(index, 1);
+          }
+          this.dtTrigger.next();
+          this.toaster.error('Respuesta ' + respuesta.Id + ' borrada');
+        });
+      }
+    });
   }
 }
