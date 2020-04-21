04c41de60d444bac2b5b855f75eb8b218bb78dba
Agulló <joseantonioagullo@gmail.com>
2020-04-21 21:49:38 +0200

    Mejoras en el navbar y notificaciones

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,6 +3,8 @@ import {Router} from '@angular/router';
 import {AutenticacionService} from '../../services/autenticacion.service';
 import {Usuario} from '../../models/usuario';
 import {UsuarioService} from '../../services/usuario.service';
+import {timer} from 'rxjs';
+import {ValidacionService} from '../../services/validacion.service';
 
 
 @Component({
@@ -12,8 +14,14 @@ import {UsuarioService} from '../../services/usuario.service';
 })
 export class NavbarComponent implements OnInit {
   public usuario: Usuario = new Usuario();
+  public toggle = false;
+  private notifier = timer(1000);
+  public notificacionesItems = 0;
+  public notificacionesPuntos = 0;
+  public notificacionesMateriales = 0;
 
-  constructor(private autenticacionService: AutenticacionService, protected usuarioService: UsuarioService, public router: Router) {
+  constructor(private autenticacionService: AutenticacionService, protected usuarioService: UsuarioService,
+              protected validacionService: ValidacionService, public router: Router) {
     this.autenticacionService.estaAutenticado();
     this.router.events.subscribe(value => {
       this.usuarioService.getLoggedUser()?.subscribe(u => {
@@ -23,10 +31,32 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.toggle = false;
+    this.notifier = timer(0);
+    this.notify();
   }
 
   logout() {
     this.autenticacionService.Logout();
+  }
+
+  notify() {
+    this.notifier.subscribe(value => {
+      this.validacionService.countAllItemsSinValidar().subscribe(c => {
+        this.notificacionesItems = c;
+      });
+      this.validacionService.countAllPuntosSinValidar().subscribe(c => {
+        this.notificacionesPuntos = c;
+      });
+      this.validacionService.countAllMaterialesSinValidar().subscribe(c => {
+        this.notificacionesMateriales = c;
+      });
+      this.notifier = timer(1000);
+      this.notify();
+    });
+  }
+
+  get checkValidations(): boolean {
+    return this.notificacionesItems + this.notificacionesPuntos + this.notificacionesMateriales > 0;
   }
 }
