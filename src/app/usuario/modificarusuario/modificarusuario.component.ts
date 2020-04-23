92352b3fb38a598194974ceac3694f5c3556d58b
ferky_27 <ferky_27@hotmail.com>
2020-04-23 22:18:10 +0200

    modificado usuariomodificar

--- src/app/usuario/modificarusuario/modificarusuario.component.ts
+++ src/app/usuario/modificarusuario/modificarusuario.component.ts
@@ -3,6 +3,8 @@ import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 import {Usuario} from '../../shared/models/usuario';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UsuarioService} from '../../shared/services/usuario.service';
+import {AutenticacionService} from '../../shared/services/autenticacion.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-modificarusuario',
@@ -13,27 +15,20 @@ export class ModificarusuarioComponent implements OnInit {
 
   formularioModificar: FormGroup;
   usuario: Usuario;
-  usuarioId: string;
-  tipousuario: string;
 
   constructor(protected route: ActivatedRoute,
               protected router: Router,
-              protected  usuarioService: UsuarioService) {
+              protected  usuarioService: UsuarioService,
+              protected autenticacionService: AutenticacionService,
+              private toaster: ToastrService) {
+    this.usuarioService.getLoggedUser().subscribe(u => {
+      this.usuario = u;
+    }, error => {
+      this.autenticacionService.Logout();
+    });
   }
 
   ngOnInit(): void {
-
-    this.route.params.subscribe(param => {
-
-      this.tipousuario = param['tipousuario'];
-      this.usuarioId = param['usuarioId'];
-
-      this.usuarioService.obtenerUsuarioPorId(this.usuarioId, this.tipousuario).subscribe(usuario => {
-        this.usuario = usuario;
-
-      });
-
-    });
 
     this.formularioModificar = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
@@ -48,11 +43,14 @@ export class ModificarusuarioComponent implements OnInit {
     this.usuario.Apellidos = this.formularioModificar.value.surname;
     this.usuario.Email = this.formularioModificar.value.email;
     console.log(this.usuario);
-    this.usuarioService.modificarUsuario(this.usuario, this.tipousuario).subscribe(
+    this.usuarioService.modificarUsuario(this.usuario, 'administrador').subscribe(
       data => {
-        this.router.navigate(['/listarusuario/' + this.tipousuario]);
+        this.toaster.success(' Usuario' + this.usuario.Id + ' modificado');
+
+        this.router.navigate(['/perfil']);
       }, error => {
-        console.log('Crear usuario admin fallido', error);
+        this.toaster.error(' Error al modificar el usuario');
+
       }
     );
   }
