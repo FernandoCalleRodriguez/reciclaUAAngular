a4a586ed8862aa50c9c9027edd570f13a9049a48
Agulló <joseantonioagullo@gmail.com>
2020-04-23 18:06:35 +0200

    Revert "Añadido notify y count validaciones home"
    This reverts commit 5cef44c2fc680188fd338acf944621f8bbeb82ac.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,6 @@ import {AutenticacionService} from '../shared/services/autenticacion.service';
 import {PuntoService} from '../shared/services/punto.service';
 import {NivelService} from '../shared/services/nivel.service';
 import {DudaService} from '../shared/services/duda.service';
-import {ValidacionService} from '../shared/services/validacion.service';
-import {timer} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -15,59 +13,25 @@ import {timer} from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  idusuario: number;
-  countUsuarios = 0;
-  countPuntos = 0;
-  countNiveles = 0;
-  countDudas = 0;
-  countItemsV = 0;
-  countMaterialesV = 0;
-  countPuntosV = 0;
-  private notifier = timer(1000);
-
+  idusuario: string;
+  countUsuarios: number;
+  countPuntos: number;
+  countNiveles: number;
+  countDudas: number;
 
 
   constructor(private usuarioService: UsuarioService,
               private autenticacionService: AutenticacionService,
               private puntoService: PuntoService,
               private nivelService: NivelService,
-              private dudaService: DudaService,
-              private validacionService: ValidacionService) {
-
-    this.autenticacionService.estaAutenticado();
-
+              private dudaService: DudaService) {
     this.usuarioService.getLoggedUser()?.subscribe(u => {
-      //this.idusuario = u.Id;
     }, error => {
       this.autenticacionService.Logout();
     });
-
-  }
-
-  ngOnInit(): void {
-    this.notifier = timer(0);
-    this.obtenerCountValidaciones();
-    this.obtenerCountCards();
-
-  }
-
-  public obtenerCountValidaciones() {
-    this.validacionService.countAllItemsSinValidar().subscribe(c => {
-      this.countItemsV = c;
+    this.usuarioService.countUsuariosWeb().subscribe(c => {
+      this.countUsuarios = c;
     });
-
-
-    this.validacionService.countAllMaterialesSinValidar().subscribe(c => {
-      this.countMaterialesV = c;
-    });
-
-
-    this.validacionService.countAllPuntosSinValidar().subscribe(c => {
-      this.countPuntosV = c;
-    });
-  }
-
-  public obtenerCountCards() {
 
     this.usuarioService.countUsuariosWeb().subscribe(c => {
       this.countUsuarios = c;
@@ -85,5 +49,10 @@ export class HomeComponent implements OnInit {
       this.countDudas = c;
     });
   }
+
+  ngOnInit(): void {
+
+  }
+
 
 }
