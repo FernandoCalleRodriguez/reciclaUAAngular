0d345c9f9ef7b4ba4040b6726c0ca3f3eebda9d4
Agulló <joseantonioagullo@gmail.com>
2020-04-30 21:09:50 +0200

    Las plantas ahora se crean desde los edificios

--- src/app/ubicacion/edificio/edificio.component.ts
+++ src/app/ubicacion/edificio/edificio.component.ts
@@ -8,6 +8,8 @@ import { ToastrService } from 'ngx-toastr';
 import { Subject } from 'rxjs';
 import { DataTableDirective } from 'angular-datatables';
 import { DtoptionsService } from '../../shared/services/dtoptions.service';
+import {PlantaService} from '../../shared/services/planta.service';
+import {Planta, PlantaEnum} from '../../shared/models/planta';
 
 @Component({
   selector: 'app-edificio',
@@ -39,7 +41,13 @@ export class EdificioComponent implements OnInit, OnDestroy {
 
   isEdit = false;
 
-  constructor(private edificioService: EdificioService, protected dtoptionsService: DtoptionsService, private autenticacionService: AutenticacionService, private toaster: ToastrService) {
+  nPlantas = 1;
+  tiposPlanta: PlantaEnum[] = null;
+  sotano;
+
+  constructor(private edificioService: EdificioService, protected dtoptionsService: DtoptionsService,
+              private autenticacionService: AutenticacionService, private toaster: ToastrService,
+              protected plantaService: PlantaService) {
     autenticacionService.estaAutenticado();
     this.dtOptions = dtoptionsService.getDtoptions('edificios');
   }
@@ -51,6 +59,8 @@ export class EdificioComponent implements OnInit, OnDestroy {
       //console.log(this.edificios);
     });
     this.edificio = new Edificio();
+
+    this.tiposPlanta = this.plantaService.getTiposPlanta();
 
   }
 
@@ -87,18 +97,31 @@ export class EdificioComponent implements OnInit, OnDestroy {
 
   submit(form: NgForm) {
     if (!this.isEdit) {
-
       //this.edificio.Id = parseInt(form.value.Id);
       this.edificio.Id = form.value.Id;
       this.edificio.Nombre = form.value.Nombre;
-
+      const fplantas = form.controls.Plantas.value;
+      const fsotano = form.controls.Sotano.value;
       this.edificioService.setEdificio(this.edificio).subscribe(res => {
         if (res != null) {
+          if (fsotano) {
+            const planta = new Planta();
+            planta.Planta = 6;
+            planta.Edificio_oid = res.Id;
+            this.plantaService.setPlanta(planta).subscribe();
+          }
+          for (let i = 1; i <= fplantas; i++) {
+            const planta = new Planta();
+            planta.Planta = i;
+            planta.Edificio_oid = res.Id;
+            this.plantaService.setPlanta(planta).subscribe();
+          }
           this.closebutton.nativeElement.click();
-
           this.toaster.success('Edificio ' + res.Id + ' creado');
           this.refresh();
         }
+      }, error => {
+        this.toaster.error('No se ha podido crear el edificio');
       });
     } else {
       this.edificioService.updateEdificio(this.edificio).subscribe(res => {
